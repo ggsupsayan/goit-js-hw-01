696a5e0111bbfc277949c71f718fdf69384b7326
Supsayan9 <127434945+Supsayan9@users.noreply.github.com>
2024-06-06 16:01:11 +0200

    first commit

---
+++ js/tusk-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+  const elementContent = parseFloat(content);
+  const elementPadding = parseFloat(padding);
+  const elementBorder = parseFloat(border);
+  const elementSum = elementContent + 2 * (elementPadding + elementBorder);
+
+  return elementSum;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
